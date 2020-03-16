fd2052396d00bcd65b2da79ac173389fd4b2895b
yuyaebean <yuyaebean@gmail.com>
2020-03-16 12:05:34 +0900

    [Header] Make Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
-function App() {
-  return <>Test</>;
+import Header from 'components/Header';
+
+export default function App() {
+  const theme = createMuiTheme({
+    palette: {
+      type: 'dark',
+      primary: {
+        main: '#f39926'
+      },
+      secondary: {
+        main: '#424242'
+      }
+    }
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+    </ThemeProvider>
+  );
 }
-
-export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    avatar: {
+      width: theme.spacing(20),
+      height: 'fit-content',
+      margin: theme.spacing(1)
+    }
+  })
+);
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position='sticky' color='secondary'>
+      <Toolbar>
+        <Avatar
+          alt='LIKE LION'
+          src='static/basiclogo_E_H_W.png'
+          variant='square'
+          className={classes.avatar}
+        />
+      </Toolbar>
+    </AppBar>
+  );
+}
